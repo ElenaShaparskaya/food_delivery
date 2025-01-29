851d79b77556a0eed7d2bdd4cb9b815f1587297e
karaseva-elena82 <karaseva-elena82@yandex.ru>
2025-01-29 15:45:26 +0300

    modal: click, close

--- script/index.js
+++ script/index.js
@@ -1,2 +1,15 @@
 const modal = document.querySelector('.cart-modal__overlay');
-console.log(modal);
+const cartBtn = document.querySelector('#cart-button');
+const cartClose = document.querySelector('#cart-close');
+
+// console.log(cartBtn);
+
+cartBtn.addEventListener('click', () => {
+    // modal.style.display = 'flex';
+    modal.classList.add('open');
+});
+
+cartClose.addEventListener('click', () => {
+    // modal.style.display = 'none';
+    modal.classList.remove('open');
+});
